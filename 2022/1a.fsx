26fae6f78a53648ff3ef806fc5cb2c025609a02e
Nathan Tolbert <tolbert@illinois.edu>
2022-12-01 14:53:00 -0600

    2022 day 1a

---
+++ 2022/1a.fsx
@@ -0,0 +1,15 @@
+let lines = List.ofSeq(System.IO.File.ReadLines("day1Data"))
+
+let summed result line =
+  if line = "" then
+    0 :: result
+  else
+    match result with
+     |  current :: rest -> current + (line |> int) :: rest
+     |  [] -> [line |> int]
+
+
+List.fold summed [] lines |> List.max
+
+
+
